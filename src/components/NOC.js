9a8027ecc5969385aa7130ce16516abb7dbcad3b
Pramukh JS <pramukh660@gmail.com>
2024-07-31 15:08:21 +0530

    NOC, SOC updated with RBA

--- src/components/NOC.js
+++ src/components/NOC.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { auth } from "./firebase";
+import { auth, db } from "./firebase"; // Ensure the correct import path
+import { doc, getDoc } from "firebase/firestore";
+import companyLogo from "../yqgzPwOk.jpg"; // Update the path to your logo
+import userIcon from "../R.png"; // Update the path to your user icon
 import "./landingPage.css";
 
 const NOC = () => {
   const navigate = useNavigate();
+  const [department, setRole] = useState(null);
+  const [userInfo, setUserInfo] = useState({ fullName: " ", email: " ", department: " " });
+  const [filteredButtonData, setFilteredButtonData] = useState([]);
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
 
   const handleBack = () => {
-    // navigate("/landing");
     window.history.back();
+  };
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      console.log("User logged out successfully!");
+      navigate("/login");
+    } catch (error) {
+      console.error("Error logging out:", error.message);
+    }
   };
 
   const handleNavigation = (path) => {
@@ -18,44 +34,101 @@ const NOC = () => {
       navigate(path);
     }
   };
-  
-  const buttonData = [
-    {
-      path: "https://172.19.2.21:8061/",
-      title: "OpManager",
-      content: "OpManager is a comprehensive network monitoring software that provides real-time visibility into the performance of various network devices, servers, and storage systems."
-    },
-    {
-      path: "https://www.zabbix.com/cloud_monitoring",
-      title: "Zabbix",
-      content: "Zabbix is an enterprise-class open source distributed monitoring solution. Zabbix is a software that monitors numerous parameters of a network and the health and integrity of servers, virtual machines, applications, services, databases, websites, the cloud and more."
-    },
-    {
-      path: "https://www.paessler.com/cloud-monitoring",
-      title: "Prtg",
-      content: "PRTG (Paessler Router Traffic Grapher ) is a network monitoring software developed by Paessler GmbH. It monitors system conditions like bandwidth usage or uptime and collect statistics from miscellaneous hosts such as switches, routers, servers, and other devices and applications."
-    },
-  ];
-  
+
+  const toggleTooltip = () => {
+    setTooltipVisible(!isTooltipVisible);
+  };
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const user = auth.currentUser;
+        if (user) {
+          const userDoc = await getDoc(doc(db, 'Users', user.uid));
+          if (userDoc.exists()) {
+            const userRole = userDoc.data().department;
+            setRole(userRole);
+
+            const buttonData = [
+              {
+                path: "https://172.19.2.21:8061/",
+                title: "OpManager",
+                content: "OpManager is a comprehensive network monitoring software that provides real-time visibility into the performance of various network devices, servers, and storage systems.",
+                department: ["admin", "user"]
+              },
+              {
+                path: "https://www.zabbix.com/cloud_monitoring",
+                title: "Zabbix",
+                content: "Zabbix is an enterprise-class open source distributed monitoring solution. Zabbix is a software that monitors numerous parameters of a network and the health and integrity of servers, virtual machines, applications, services, databases, websites, the cloud and more.",
+                department: ["admin"]
+              },
+              {
+                path: "https://www.paessler.com/cloud-monitoring",
+                title: "Prtg",
+                content: "PRTG (Paessler Router Traffic Grapher) is a network monitoring software developed by Paessler GmbH. It monitors system conditions like bandwidth usage or uptime and collects statistics from miscellaneous hosts such as switches, routers, servers, and other devices and applications.",
+                department: ["admin", "user"]
+              },
+            ];
+
+            const filteredData = buttonData.filter(button => button.department.includes(userRole));
+            setFilteredButtonData(filteredData);
+
+            setUserInfo({
+              fullName: userDoc.data().fullName,
+              email: user.email,
+              department: userRole
+            });
+          } else {
+            console.error("No such document!");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user role:", error.message);
+      }
+    };
+
+    fetchUserRole();
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  if (department === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="landing-page-container">
-      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */}
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-      <button className="back-button" onClick={handleBack}><i class="material-icons">arrow_back</i> Back</button>
-      <h1>NOC Management</h1>
+      <div className="header">
+        <img src={companyLogo} alt="Company Logo" className="company-logo" />
+        <h1 className="company-name">Netcon</h1>
+        <div className="user-section" onClick={toggleTooltip}>
+          <img src={userIcon} alt="User Icon" className="user-icon" />
+          {isTooltipVisible && (
+            <div className="user-info-tooltip">
+              <p>{userInfo.fullName}</p>
+              <p>{userInfo.email}</p>
+              <p>{userInfo.department}</p>
+              <button className="logout-button" onClick={handleLogout}>Logout</button>
+            </div>
+          )}
+        </div>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+        <button className="back-button" onClick={handleBack}><i className="material-icons">arrow_back</i> Back</button>
+      </div>
       <br />
-      <div className="buttons-container">
-        {buttonData.map((button, index) => (
-          <div key={index} className="button-wrapper" onClick={() => handleNavigation(button.path)}>
+      <div className="content">
+        <h2>NOC</h2>
+        <br />
+        {filteredButtonData.map((button, index) => (
+          <div key={index} className="button-wrapper">
             <h4>{button.title}</h4>
             <p>{button.content}</p>
-            {/* <button onClick={() => handleNavigation(button.path)}>Click here</button> */}
+            <button onClick={() => handleNavigation(button.path)}>
+              Open {button.title}
+            </button>
           </div>
         ))}
       </div>
     </div>
   );
-  
 };
 
 export default NOC;
--- src/components/SOC.js
+++ src/components/SOC.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { auth, db } from "./firebase"; // Ensure the correct import path
+import { doc, getDoc } from "firebase/firestore";
 import companyLogo from "../yqgzPwOk.jpg"; // Update the path to your logo
 import userIcon from "../R.png"; // Update the path to your user icon
 import "./landingPage.css";
 
 const SOC = () => {
   const navigate = useNavigate();
+  const [department, setRole] = useState(null);
+  const [userInfo, setUserInfo] = useState({ fullName: " ", email: " ", department: " " });
+  const [filteredButtonData, setFilteredButtonData] = useState([]);
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
 
   const handleBack = () => {
-    // navigate("/landing");
     window.history.back();
+  };
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      console.log("User logged out successfully!");
+      navigate("/login");
+    } catch (error) {
+      console.error("Error logging out:", error.message);
+    }
   };
 
   const handleNavigation = (path) => {
@@ -20,41 +35,84 @@ const SOC = () => {
     }
   };
 
-  const buttonData = [
-    {
-      path: "https://wazuh.com/cloud/",
-      title: "Wazuh",
-      content:
-        "Wazuh is a free and open source platform used for threat prevention, detection, and response. It is capable of protecting workloads across on-premises, virtualized, containerized, and cloud-based environments.Wazuh solution consists of an endpoint security agent, deployed to the monitored systems, and a management server, which collects and analyzes data gathered by the agents.",
-    },
-  ];
+  const toggleTooltip = () => {
+    setTooltipVisible(!isTooltipVisible);
+  };
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const user = auth.currentUser;
+        if (user) {
+          const userDoc = await getDoc(doc(db, 'Users', user.uid));
+          if (userDoc.exists()) {
+            const userRole = userDoc.data().department;
+            setRole(userRole);
+
+            const buttonData = [
+              {
+                path: "https://wazuh.com/cloud/",
+                title: "Wazuh",
+                content:
+                  "Wazuh is a free and open source platform used for threat prevention, detection, and response. It is capable of protecting workloads across on-premises, virtualized, containerized, and cloud-based environments.Wazuh solution consists of an endpoint security agent, deployed to the monitored systems, and a management server, which collects and analyzes data gathered by the agents.",
+                department: ["admin"]
+              },
+            ];
+
+            const filteredData = buttonData.filter(button => button.department.includes(userRole));
+            setFilteredButtonData(filteredData);
+
+            setUserInfo({
+              fullName: userDoc.data().fullName,
+              email: user.email,
+              department: userRole
+            });
+          } else {
+            console.error("No such document!");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user role:", error.message);
+      }
+    };
+
+    fetchUserRole();
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  if (department === null) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="landing-page-container">
       <div className="header">
         <img src={companyLogo} alt="Company Logo" className="company-logo" />
         <h1 className="company-name">Netcon</h1>
-        <div className="user-section">
+        <div className="user-section" onClick={toggleTooltip}>
           <img src={userIcon} alt="User Icon" className="user-icon" />
-          {/* <button className="logout-button" onClick={handleBack}>
-            <i className="material-icons">arrow_back</i>
-          </button> */}
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-          <button className="back-button" onClick={handleBack}><i class="material-icons">arrow_back</i> Back</button>
-
+          {isTooltipVisible && (
+            <div className="user-info-tooltip">
+              <p>{userInfo.fullName}</p>
+              <p>{userInfo.email}</p>
+              <p>{userInfo.department}</p>
+              <button className="logout-button" onClick={handleLogout}>Logout</button>
+            </div>
+          )}
         </div>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+        <button className="back-button" onClick={handleBack}><i className="material-icons">arrow_back</i> Back</button>
       </div>
-      {/* <h2>SOC Management</h2> */}
       <br />
-      <div className="buttons-container">
-        {buttonData.map((button, index) => (
-          <div
-            key={index}
-            className="button-wrapper"
-            onClick={() => handleNavigation(button.path)}
-          >
+      <div className="content">
+        <h2>NOC</h2>
+        <br />
+        {filteredButtonData.map((button, index) => (
+          <div key={index} className="button-wrapper">
             <h4>{button.title}</h4>
             <p>{button.content}</p>
+            <button onClick={() => handleNavigation(button.path)}>
+              Open {button.title}
+            </button>
           </div>
         ))}
       </div>
